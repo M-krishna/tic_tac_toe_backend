1b84aa387b2201060e9dc18e7f9b6ecda1f059c6
Krishna Murugan <murugesan@think42labs.com>
2019-07-28 19:30:44 +0530

    (fix.)

--- tic_tac_toe/settings.py
+++ tic_tac_toe/settings.py
@@ -163,8 +163,8 @@ AUTH_USER_MODEL = 'authentication.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'krishnamurugan589@gmail.com'
-EMAIL_HOST_PASSWORD = 'Morpheous1997'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 
